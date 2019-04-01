e5a6c8379501a3afb500d0e57880abafc123cfcd
syenna.graham@gmail.com <syenna.graham@gmail.com>
2019-03-31 21:03:28 -0400

    change file names

---
+++ container_scanning_client/src/config.js
@@ -0,0 +1,17 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "container-scanning-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://k70sekcbu6.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_qJP1adFWo",
+    APP_CLIENT_ID: "2fq8geje8ptcp8ua8b5kur0mji",
+    IDENTITY_POOL_ID: "us-east-1:f6317782-c75e-4099-8bf3-c0f12bd9cb38"
+  }
+};
+
